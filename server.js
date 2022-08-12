5f4e2e38f8cf879d593fa78c897882e7dc059389
Andrey Rudinskiy <drerudin@gmail.com>
2022-08-12 16:44:19 -0700

    added starter code

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fs = require("fs");
+const notes = require("./Develop/db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+// Middleware app
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static())
