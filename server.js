18aa496352f7b3bde521a408a0478446ded001d9
Andrey Rudinskiy <drerudin@gmail.com>
2022-08-20 01:23:39 -0700

    more issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require("fs");
-const notes = require("../db/db.json");
+const notes = require("./db/db.json");
 const path = require("path");
 const uuid = require("uuid");
 const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
