f9664471877f9f9be5b41ee064aded4e1656a054
Andrey Rudinskiy <drerudin@gmail.com>
2022-08-28 21:10:34 -0700

    final changes

--- server.js
+++ server.js
@@ -1,50 +1,83 @@
 const express = require('express');
-const fs = require("fs");
-const notes = require("./db/db.json");
-const path = require("path");
-const uuid = require("uuid");
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
-const { application } = require('express');
-
-
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
 const app = express();
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-// Middleware app
-app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));``
 
-app.get("/notes", (req, res) => {
-  res.sendFile(__dirname + "/notes.html")
-});
+app.get('/'), (req, res) => 
+res.sendFile(path.join(__dirname, './public/index.html'));
 
-app.get("/api/notes", (req, res) => {
-  let notes = fs.readFileSync("db/db.json");
-  let data = notes.toString();
-  res.json(JSON.parse(data));
-});
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, './public/notes.html')));
 
-app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"))
-} );
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+    });
 
-app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
-  newNote.id = uuid.v4();
-  notes.push(newNote);
-  fs.writeFileSync("db/db.json", JSON.stringify(notes));
-  res.json(newNote);
-} );
+app.get('/api/notes/:id', (req, res) => {
+    if (req.body && req.params.id) {
+        const Id = req.params.id;
+        for (let i = 0; i < notes.length; i++) {
+            if (notes.id === Id) {
+                res.json(notes[i]);
+                return;
+            }
+        }
+        res.json('No note found');
+    }
+}
+);
 
-app.delete("/api/notes/:id", (req, res) => {
-  let id = req.params.id;
-  let newNotes = notes.filter(note => note.id !== id);
-  fs.writeFileSync("db/db.json", JSON.stringify(newNotes));
-  res.json(newNotes);
-} );
+app.post('/api/notes', (req, res) => {
+    req.body.id = Math.floor(Math.random() * 1000000).toString()
+    note = {
+        id: req.body.id,
+        title: req.title,
+        text: req.body.text
+    }
+    notes.push(req.body)
+    res.json(notes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+}
+);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-} );
+// delete a note using the id
+app.delete('/api/notes/:id', (req, res) => {
+    const Id = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === Id) {
+            notes.splice(i, 1);
+            res.json(notes);
+            fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+            return;
+        }
+    }
+    res.json('No note found');
+}
+);
 
+// uses handleNewNoteView to update note in the db
+app.put('/api/notes/:id', (req, res) => {
+    const Id = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === Id) {
+            notes[i].title = req.body.title;
+            notes[i].text = req.body.text;
+            res.json(notes);
+            fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+            return;
+        }
+    }
+    res.json('No note found');
+}
+);
+
+
+
+// listen for requests :)
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
